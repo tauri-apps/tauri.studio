2c856c4ee8a631df43e4794615af8d21a6b5a86c
Denjell <denjell@sfosc.org>
2020-02-21 16:51:07 +0100

    push dist

---
+++ docs-generator/dist/ssr/www/precache-manifest.c3e8e37b57b11df1a6e9570a2607e2f6.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44c4a15ac994271e083",
+    "url": "/css/2.ea3395ae.css"
+  },
+  {
+    "revision": "251af02453709d46b460",
+    "url": "/css/3.e51c852e.css"
+  },
+  {
+    "revision": "0b7bfbdefcfe7ba39b88",
+    "url": "/css/4.eafea247.css"
+  },
+  {
+    "revision": "5b9ce5c349d3513069e0",
+    "url": "/css/5.f07aa5cc.css"
+  },
+  {
+    "revision": "44260f0f150070f1dbeb",
+    "url": "/css/6.7bdfacea.css"
+  },
+  {
+    "revision": "e10f5503cf608d1d8ba0",
+    "url": "/css/7.e51c852e.css"
+  },
+  {
+    "revision": "1fa873dc9bbd326edee7",
+    "url": "/css/app.0ce808d5.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/fonts/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/fonts/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/fonts/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/fonts/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/fonts/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/fonts/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/fonts/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "c44c4a15ac994271e083",
+    "url": "/js/2.1b117f18.js"
+  },
+  {
+    "revision": "251af02453709d46b460",
+    "url": "/js/3.a2d3ab0e.js"
+  },
+  {
+    "revision": "0b7bfbdefcfe7ba39b88",
+    "url": "/js/4.3a606182.js"
+  },
+  {
+    "revision": "5b9ce5c349d3513069e0",
+    "url": "/js/5.74a045f0.js"
+  },
+  {
+    "revision": "44260f0f150070f1dbeb",
+    "url": "/js/6.dff540ce.js"
+  },
+  {
+    "revision": "e10f5503cf608d1d8ba0",
+    "url": "/js/7.27b3e441.js"
+  },
+  {
+    "revision": "8422aab96f689419197b",
+    "url": "/js/8.7b3fc61b.js"
+  },
+  {
+    "revision": "137b2235805e700287b1",
+    "url": "/js/9.613bac60.js"
+  },
+  {
+    "revision": "1fa873dc9bbd326edee7",
+    "url": "/js/app.f36fd624.js"
+  },
+  {
+    "revision": "db98b19921d974fd174a",
+    "url": "/js/vendor.4683bb0c.js"
+  },
+  {
+    "revision": "ed1a64715056e6002846466645f6365d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "764b83d05e03c5cb521e2f2383823d45",
+    "url": "/statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "7241b689ad55ad2fbd6c20b40c89e59b",
+    "url": "/statics/discord-light.svg"
+  },
+  {
+    "revision": "e9ec13b3de0578a2e387b2919de4c6df",
+    "url": "/statics/discord.svg"
+  },
+  {
+    "revision": "1b80415d70a21d7182eb3d16b67e3a18",
+    "url": "/statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "7580d79b411cb007f5687257bc1bf81f",
+    "url": "/statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "b7a53e220ed44688073a7d70814000bc",
+    "url": "/statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "70105c0bd8a35dc9a6db0d9b7389246a",
+    "url": "/statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "f820c566694348ed7f207a9766af2f85",
+    "url": "/statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "6ec252210305a1998fb564d76cf3f5de",
+    "url": "/statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "9057402186d23c24f143ba8de1acd485",
+    "url": "/statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "c3ba534ca5f81797943eb492df844f00",
+    "url": "/statics/icons/favicon.ico"
+  },
+  {
+    "revision": "764b83d05e03c5cb521e2f2383823d45",
+    "url": "/statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "615212c33148979a6797dceeca1a164a",
+    "url": "/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "a43cff8d60e693bdfd9d9ea39ae106e2",
+    "url": "/statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "7084c7f51108da696fc8de013f22523c",
+    "url": "/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "9d41287d34398803fe1e0df005a4c7a7",
+    "url": "/statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "b5bb296ac748d11835f002611ac44469",
+    "url": "/statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "424df4e15da848701d5267477913dc37",
+    "url": "/statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "a6a603c39187b437b16e695c968544ee",
+    "url": "/statics/images/bg-slice.png"
+  },
+  {
+    "revision": "8df77bdd4eeee2279107ba501319543f",
+    "url": "/statics/images/bigben.jpg"
+  },
+  {
+    "revision": "78d156d8d58abd3f8ad716f296f068db",
+    "url": "/statics/images/boat.jpg"
+  },
+  {
+    "revision": "cdb469468cb47f7620d3528621da8284",
+    "url": "/statics/images/bookCover.png"
+  },
+  {
+    "revision": "f018b8ff800b64dc354d4a7fa133f5c0",
+    "url": "/statics/images/feather.jpg"
+  },
+  {
+    "revision": "f9fad6ac1e754f9137132ba06746d417",
+    "url": "/statics/images/hammers.jpg"
+  },
+  {
+    "revision": "e60f745799a41f9a8437801c2dadacca",
+    "url": "/statics/images/keys.jpg"
+  },
+  {
+    "revision": "c3d02c7fff37feee59e3b5db1e8b8f1f",
+    "url": "/statics/images/lightning.jpg"
+  },
+  {
+    "revision": "cf4eed26830bd280a8540049fe226e40",
+    "url": "/statics/images/locks.jpg"
+  },
+  {
+    "revision": "5d4d37cb346b0e0438752f093424d9cb",
+    "url": "/statics/images/skycave.jpg"
+  },
+  {
+    "revision": "1ca0a5aba893a9028b34af8bd3554702",
+    "url": "/statics/images/snowflake.jpg"
+  },
+  {
+    "revision": "40d7f2945d87d17d01cfc6c10c2b8114",
+    "url": "/statics/images/vegetables.jpg"
+  },
+  {
+    "revision": "0a8f92907cd9ccd58848dc0e60f60582",
+    "url": "/statics/open-collective-light.svg"
+  },
+  {
+    "revision": "eea525077d0bc3873609945ef5f522ed",
+    "url": "/statics/open-collective.svg"
+  },
+  {
+    "revision": "ed41ac63ecc6b690ee631e89712f4818",
+    "url": "/statics/partnerLogos/IOTA_Logo_black_100px.png"
+  },
+  {
+    "revision": "ca3bbba90b509a1fc072524e4e9c632a",
+    "url": "/statics/partnerLogos/IOTA_Logo_white_100px.png"
+  },
+  {
+    "revision": "8efd6b27c5b291d8926fa32bae6f9f06",
+    "url": "/statics/partnerLogos/amethyst-standard.svg"
+  },
+  {
+    "revision": "8f68cb34990a74ba93b83abd02769b6c",
+    "url": "/statics/partnerLogos/amethyst-white.svg"
+  },
+  {
+    "revision": "8efd6b27c5b291d8926fa32bae6f9f06",
+    "url": "/statics/partnerLogos/amethyst.svg"
+  },
+  {
+    "revision": "d1965a957f2ce1e1a7771b0c7d3fe672",
+    "url": "/statics/partnerLogos/guijs-round.svg"
+  },
+  {
+    "revision": "3006ed8dfd31838b4b14b72a44d84d08",
+    "url": "/statics/partnerLogos/meros.png"
+  },
+  {
+    "revision": "58148e7d58c88f77be29f5f1c9f37d8a",
+    "url": "/statics/partnerLogos/pureos.png"
+  },
+  {
+    "revision": "36ae4c5d3266a0144a77fc281b92a1e4",
+    "url": "/statics/partnerLogos/tensor.png"
+  },
+  {
+    "revision": "76f57892a2833ca1c4238d1a397c49b7",
+    "url": "/statics/partnerLogos/wallabyjs.png"
+  },
+  {
+    "revision": "37ae4df61df6e2e8ddac46e5fb731c70",
+    "url": "/statics/patterns/Bridge.png"
+  },
+  {
+    "revision": "2257393830110573e7d68615e741d399",
+    "url": "/statics/patterns/Cloudbridge.png"
+  },
+  {
+    "revision": "4130cbebcae7e6a5b43ac7aaa36f397c",
+    "url": "/statics/patterns/Cloudish.png"
+  },
+  {
+    "revision": "42abcdb74f59e020e09573cdde37f6f3",
+    "url": "/statics/patterns/GLUI.png"
+  },
+  {
+    "revision": "82abc0d74e0e83a76cab72131ab69405",
+    "url": "/statics/patterns/Hermit.png"
+  },
+  {
+    "revision": "cd9ed4d7de8c4eb58934157be505d22b",
+    "url": "/statics/patterns/Kamikaze.png"
+  },
+  {
+    "revision": "279c9b3248b6a693e88f581ce796b8f6",
+    "url": "/statics/patterns/Multiwin.png"
+  },
+  {
+    "revision": "f09f36b3de0d8d4ce625b297e71a4548",
+    "url": "/statics/tauri-studio.svg"
+  },
+  {
+    "revision": "fcb79b4883b4b237012b3c83be65752d",
+    "url": "/statics/tauri-text.png"
+  },
+  {
+    "revision": "93e03d04b2445911ecf5cbe2d1bd28c2",
+    "url": "/statics/tauri-wordmark.png"
+  },
+  {
+    "revision": "3d091b26bf50048f26576e571a7a8679",
+    "url": "/statics/tauri.svg"
+  },
+  {
+    "revision": "fd4c87efeafb1de10e5db3e9a0f94ce5",
+    "url": "/statics/thetaTauri_logo.png"
+  }
+]);
---
+++ docs-generator/dist/ssr/www/service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.c3e8e37b57b11df1a6e9570a2607e2f6.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "docs-generator"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {
+  "directoryIndex": "/"
+});
+
+workbox.routing.registerRoute("/", new workbox.strategies.NetworkFirst(), 'GET');
+workbox.routing.registerRoute("/statics/*", new workbox.strategies.CacheFirst(), 'GET');
